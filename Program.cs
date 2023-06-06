c4a30dda9c0cdcb42cd30d2562b2e05375e10021
Simran Bhagnani <Simran.Bhagnani@royalmail.com>
2023-06-06 14:38:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () => "Hello world!");
+
+app.Run();
